d54bc1fac030cc98ef83d5f85618d4f9df5f1294
Francisco Macedo <francarmando53@gmail.com>
2026-02-03 23:14:09 +0100

    Teste com output js feitos com sucesso

--- modulo_basico/js/aula02_output_do_js.js
+++ modulo_basico/js/aula02_output_do_js.js
@@ -1,3 +1,5 @@
+document.getElementById('p_teste').innerHTML = "Sejam todos bem vindo aos primeiros testes. <hr>"
+// document.getElementById('p_teste').innerText = "Sejam todos bem vindo aos primeiros testes. <hr>"
 document.write("Hello world!")
 
 let nome = ''
