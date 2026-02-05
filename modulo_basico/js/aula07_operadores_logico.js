ff47882906b38f1ce41fb1c496b2d874ae5076c7
Francisco Macedo <francarmando53@gmail.com>
2026-02-05 18:12:56 +0100

    Testes feito com operadores logico

--- modulo_basico/js/aula07_operadores_logico.js
+++ modulo_basico/js/aula07_operadores_logico.js
@@ -1,3 +1,21 @@
 /*
     *NESTA AULA VAMOS FALAR SOBRE 
+    * TIPOS DE OPERADORES
+    * &&    AND  PRECISA QUE AS DUAS EXPRESSOES SEJAM VERDADEIRAS
+    * ||    OR   PRECISA QUE PELO UMA DAS EXPRESSOES SEJA VERDADEIRA   
+    * !     NOT  NEGAÇÃO
+    * ??
 */
+
+let v1 = 4
+let v2 = 5
+let v3 = 6
+
+console.log("------------------ && ------------------")
+console.log(v1 < v2 && v2 < v3) //true
+
+console.log("------------------ || ------------------")
+console.log(v2 > v1 || v1 > v3) //true
+
+console.log("------------------ ! ------------------")
+console.log(!(v1 > v3 || v2 > v1)) // now it will be false
