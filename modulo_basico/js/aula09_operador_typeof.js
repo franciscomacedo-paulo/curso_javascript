5e704e73bc6e6e6cf9f77c92f651beb5a9df30b2
Francisco Macedo <francarmando53@gmail.com>
2026-02-05 21:14:17 +0100

    Mais um teste feito com sucesso

--- modulo_basico/js/aula09_operador_typeof.js
+++ modulo_basico/js/aula09_operador_typeof.js
@@ -10,7 +10,11 @@ numF = 5.1
 str = "Lurdes"
 logico = true
 
+
 console.log(numI + " é do tipo " + typeof(numI) )
 console.log(numF + " é do tipo " + typeof(numF) )
 console.log(str + " é do tipo " + typeof(str) )
-console.log(logico + " é do tipo " + typeof(logico) )
+console.log(logico + " é do tipo " + typeof(logico))
+
+const verif = (typeof(numI) === 'number') ? "Sim é realmente um número" : "Infelizamente não é um número!" 
+console.log(verif)
