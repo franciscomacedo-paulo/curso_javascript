38e6377c5afcf120a04f336e5435213ddbc1cc2f
Francisco Macedo <francarmando53@gmail.com>
2026-02-05 20:51:23 +0100

    Teste feito com sucesso

---
+++ modulo_basico/js/aula08_operador_ternario.js
@@ -0,0 +1,7 @@
+/*
+    *NESTA AULA VAMOS FALAR DO OPERADOR TERNARIO, QUE É UMA FORMA SIMPLICADA DO OPERADOR RELACIONAL IF...ELSE 
+*/
+let idade = 18
+let stado = "a"
+let verificador = (idade >= 18 && stado == "A" ) ? "Acesso Permitido" : "Acesso Negato";
+console.log(verificador)
