0881d25e62ace31d7d47543b7c450aa2193ee27d
Francisco Macedo <francarmando53@gmail.com>
2026-02-05 21:01:31 +0100

    Operador typeof em js

---
+++ modulo_basico/js/aula09_operador_typeof.js
@@ -0,0 +1,11 @@
+"use strict"
+
+/*
+    *NESTA AULA VAMOS FALAR SOBRE O OPERADOR TYPEOF
+    *Usado para mostrar o tipo de uma variável, ou seja o tipo primitivo
+*/
+let numI, numF, str, logico
+numI = 10
+numF = 5.1
+str = "Lurdes"
+logico = true
