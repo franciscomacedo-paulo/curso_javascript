c810cf592e92c547e73fdfe0d8926052f3f1b5cb
Francisco Macedo <francarmando53@gmail.com>
2026-02-05 13:28:57 +0100

    Teste feitos com operdadores aritmétiocs

--- modulo_basico/js/aula05_operadore_aritmeticos.js
+++ modulo_basico/js/aula05_operadore_aritmeticos.js
@@ -13,3 +13,23 @@
  * -- DECREMENTO
 */
 
+/*
+    *EXEMPLOS DE USO DOS OPERDAORES
+*/
+/* 
+    *Soma  
+*/
+let num_1 = 2
+let num_2 = 4
+let solve = num_1 + num_2
+console.log(solve)
+
+let n = 3
+let m = 6
+console.log(n - m)
+
+let v1 = 2
+let v2 = 4
+let v3 = 2 ** 4
+console.log(v3)
+
