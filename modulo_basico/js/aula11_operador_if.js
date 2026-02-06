94f64eb1b47509d3878bec0614a26a80f3bf3479
Francisco Macedo <francarmando53@gmail.com>
2026-02-06 13:52:48 +0100

    Testes feito com sucesso

--- modulo_basico/js/aula11_operador_if.js
+++ modulo_basico/js/aula11_operador_if.js
@@ -7,4 +7,26 @@ if((typeof(nome) === 'string' && typeof(num) === 'number') && (typeof(logico) ==
     console.log("Todos os tipos correspondem devidamente")
 }else{
     console.log("Os tipos não combinam!")
+
 }
+console.log("********************************************")
+
+/*
+    * DETETOR DE VOLOCIDADE MÁXIMA
+*/
+const maxVelo = 120;
+const minVelo = 60;
+const zonaPropri = "bairro"
+
+if(maxVelo == 120 && zonaPropri == "estrada"){
+    console.log("Siga a sua viagem com segurança")
+}else if(maxVelo > 120 && zonaPropri == "bairro"){
+    console.log("Reduza a velocidade dentro das localidades")
+}
+
+// if(maxVelo > 120 && zonaRuaral == "Bairro"){
+//     console.log("Por favor reduza a velocidade")
+
+// }else if(maxVelo >= 120 && zonaPropri == "Estrada"){
+//     console.log("Podes seguir a tua viagem mais com prudencia")
+// }
