b374f20a2fa87df68f45a53bc5c1f0390d71469e
Francisco Macedo <francarmando53@gmail.com>
2026-02-07 20:15:11 +0100

    Teste feitos com for normal

---
+++ modulo_basico/js/aula13_Loop_for_tipo.js
@@ -0,0 +1,31 @@
+/*
+    *VAMOS CONTAR DE 1 ATE 10, USANDO O LOOP FOR
+*/
+
+for(let i = 1;i < 10;i++){
+    console.log(i)
+}
+
+// ENCONTRANDO NUMEROS MENORES
+console.log("*****************************")
+let num_menor = 0
+for(let i = 0;i < 20;i++){
+    if(i < 10){
+        num_menor ++
+    }
+}
+console.log(num_menor)
+console.log("*****************************")
+//PROCURANDO VALORES PARES E IMPAR
+let odd = 0 //valore impares
+let even = 0 // valores par
+
+for(let i = 1;i <= 15;i++){
+    if(i % 2 === 0){
+        even++
+    }else{
+        odd++
+    }
+}
+console.log("A quantidade de valores par encontrado foi de: " + even)
+console.log("A quantidade de valores impar encontrado foi de: "+ odd)
