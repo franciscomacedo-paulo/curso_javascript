fe6c54c6ce70e59bd48fe15cd4bd85532128ebb5
Francisco Macedo <francarmando53@gmail.com>
2026-02-07 19:31:45 +0100

    Testes feito com sucesso

---
+++ modulo_basico/js/aula12_switch_case.js
@@ -0,0 +1,47 @@
+let week_days = "sunday"
+
+/*
+    *PRIMEIRA VERSÃO USANDO IF...ELSE
+*/
+if(week_days === 'monday'){
+    console.log("Dia normal de trabalho dentro do if")
+
+}else if(week_days === 'tuesday'){
+    console.log("Esse é um outro dia normal de trablho meu caro dentro do if")
+
+}else if(week_days === 'wednesday'){
+    console.log("Bora trabalhar seu preguisozo dentro do if")
+
+}else if(week_days === 'thursday'){
+    console.log("Bora que atraz vem gente dentro do if")
+
+}else if(week_days === 'friday'){
+    console.log("Chegou o dia do homem dentro do if")
+
+}else{
+    console.log("Opa! esse dia não exsite não hein dentro do if")
+}
+
+/*
+    *SEGUNDA VERSÃO USANDO SWITCH...CASE
+*/
+console.log("****************************************************************************")
+switch(week_days){
+    case 'monday':
+        console.log("Dia normal de trabalho dentro do switch")
+        break
+    case 'tuesday':
+        console.log("Esse é um outro dia normal de trablho meu caro dentro do switch")
+        break
+    case 'wednesday':
+        console.log("Bora trabalhar seu preguisozo dentro do switch")
+        break
+    case 'thursday':
+        console.log("Bora que atraz vem gente dentro do switch")
+        break
+    case 'friday':
+        console.log("Chegou o dia do homem dentro do switch")
+        break
+    default:
+        console.log("Opa! esse dia não exsite não hein dentro do switch")
+}
