d07fd529f3262ec56be6ee2f85a7ed3614c4ba77
Francisco Macedo <francarmando53@gmail.com>
2026-02-07 23:14:29 +0100

    Testes realizado com loop do while

---
+++ modulo_basico/js/aula15_do_while.js
@@ -0,0 +1,11 @@
+/*
+    *NESTA AULA VAMOS VER SOBRE O LOOP DO WHILE (FAÇA ENQUANTO)
+    * O CODIGO SERÁ EXECUTADO PELO MENOS UMA VEZ
+*/
+
+let i = 10
+
+do{
+    console.log("Paulo")
+    i++
+}while(i < 10)
