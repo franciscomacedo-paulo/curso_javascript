878204d5f7f7523f9d0f06431c889e7cf7e17942
Francisco Macedo <francarmando53@gmail.com>
2026-02-08 16:19:54 +0100

    Prática um feita com sucesso

---
+++ modulo_basico/js/aula16_com_loops.js
@@ -0,0 +1,11 @@
+/*
+    *ENCONTRANDO O MAIOR E MENOR NÚMERO DA LISTA 
+ */
+const list_number_1 = [12,34,54,4,56,67,11]
+const list_number_2 = [10,4,5,6,7,89,1,22,45,11]
+const list_number_3 = [...list_number_1,...list_number_2]
+console.log(list_number_3)
+
+for(n of list_number_3){
+    console.log(n)
+}
