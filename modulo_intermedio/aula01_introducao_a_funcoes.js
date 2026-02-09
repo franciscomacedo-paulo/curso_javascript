0caab1dca7a00a8447d5590ec45f45529341f780
Francisco Macedo <francarmando53@gmail.com>
2026-02-09 20:14:24 +0100

    Teste feitos com funcoes primeira aula

---
+++ modulo_intermedio/aula01_introducao_a_funcoes.js
@@ -0,0 +1,17 @@
+"use strict"
+
+/*
+    *NESTA AULA VAMOS DAR OS PRIMEIRO PASSOS NOS ASSUNTOS SOBRE FUNCOES
+*/
+//* SINTAXE
+
+function nome_da_funcao(){
+    console.log("Você vai conseguir, eu sei que vais")
+}
+//nome_da_funcao()
+
+//* CHAMANDO A FUNCAO DENTRO DE UM LOOP
+
+for(let i = 1;i < 5;i++){
+    nome_da_funcao()
+}
