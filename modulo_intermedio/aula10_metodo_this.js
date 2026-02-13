a3543c9f3179367e229ebd34efc64ec8960c9c7b
Francisco Macedo <francarmando53@gmail.com>
2026-02-13 14:08:11 +0100

    Testes realizado com o opweador This

--- modulo_intermedio/aula10_metodo_this.js
+++ modulo_intermedio/aula10_metodo_this.js
@@ -1,2 +1,26 @@
 //** NESTA AULA VAMOS VER OS USO DO OPERADOR THIS EM JAVASCRIPT 
 
+function aluno(nome, nota){
+    this.nome
+    this.nota
+
+    return `${nome} ${nota}`
+}
+console.log(aluno('Alexa',9.1))
+
+const pessoa = {
+    nome: "Macedo",
+
+    falar:function(){
+        console.log("Estou corrento " + this.nome)
+    }
+}
+pessoa.falar()
+
+const dog = {
+    nome: "Lupi",
+
+    latir: function(){
+        console.log("O cachorro esta latindo " + this.nome)
+    }
+}
