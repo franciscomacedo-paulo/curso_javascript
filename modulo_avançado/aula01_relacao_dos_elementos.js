929554ec8bf8f4c8bf900330b57d0a85ff9bf14d
Francisco Macedo <francarmando53@gmail.com>
2026-02-22 21:01:04 +0100

    Testes realizado e concluidos com sucesso

---
+++ modulo_avançado/aula01_relacao_dos_elementos.js
@@ -0,0 +1,19 @@
+const div_pai = document.querySelector('.container')
+const cursos = document.querySelectorAll('.item')
+const corpo = document.querySelector('body')
+
+console.log(div_pai.children) // Propriedade pra ver todos os filhos de um elemento em especifico
+
+console.log(div_pai.firstElementChild)
+console.log(div_pai.lastElementChild)
+console.log(corpo.children)
+
+// Verificando se um elemento tem filhos
+console.log(div_pai.hasChildNodes())
+
+let verificar  = cursos[0].hasChildNodes ? "possui filho" : "nao possui filho"
+console.log(verificar)
+
+//Verificando o pai do elemento em uma posicao especifica
+
+console.log(cursos[0].parentElement.parentElement.parentElement) //aqui estou a pegar ate o bisavo
