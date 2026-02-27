48c92dc0a7396070f6a22a91a7467fe8f9a262f5
Francisco Macedo <francarmando53@gmail.com>
2026-02-27 12:43:56 +0100

    Primeiros teste feitos com sucesso

--- modulo_avançado/aula04_metodo_filter.js
+++ modulo_avançado/aula04_metodo_filter.js
@@ -1,20 +1,97 @@
 //* NESTA AULA VAMOS FALAR DO METODO FILTER
-const idades = [12,34,54,10,18,21,78,7,8,9,13]
-console.log(idades)
-console.log("******************************************")
+const idades = [12, 34, 54, 10, 18, 21, 78, 7, 8, 9, 13];
+console.log(idades);
+console.log("******************************************");
 
+const produtos = [
+  {
+    id: 1,
+    nome: "Smartphone Dalaxy S21",
+    preco: 399.99,
+    temDesconto: true,
+    qtdProduto: 1,
+  },
+  {
+    id: 2,
+    nome: "Notebook Dell Inspiron",
+    preco: 4500.0,
+    temDesconto: false,
+    qtdProduto: 3,
+  },
+  {
+    id: 3,
+    nome: "Smart TV LG 55",
+    preco: 2799.0,
+    temDesconto: true,
+    qtdProduto: 5,
+  },
+  {
+    id: 4,
+    nome: "Fone de Ouvido Bluetooth JBL",
+    preco: 299.0,
+    temDesconto: false,
+    qtdProduto: 2,
+  },
+  {
+    id: 5,
+    nome: "Câmera DSLR Canon",
+    preco: 3200.0,
+    temDesconto: true,
+    qtdProduto: 1,
+  },
+  {
+    id: 6,
+    nome: "Tablet iPad Air",
+    preco: 4100.0,
+    temDesconto: false,
+    qtdProduto: 8,
+  },
+  {
+    id: 7,
+    nome: "Console PlayStation 5",
+    preco: 4699.0,
+    temDesconto: true,
+    qtdProduto: 2,
+  },
+  {
+    id: 8,
+    nome: "Smartwatch Apple Watch",
+    preco: 2499.0,
+    temDesconto: false,
+    qtdProduto: 7,
+  },
+  {
+    id: 9,
+    nome: "Impressora HP Multifuncional",
+    preco: 599.0,
+    temDesconto: true,
+    qtdProduto: 5,
+  },
+  {
+    id: 10,
+    nome: "Caixa de Som POrtátil Sony",
+    preco: 1000.0,
+    temDesconto: false,
+    qtdProduto: 6,
+  },
+];
 
-const novasIdade = idades.filter((el) =>{
-    if(el >= 18){
-        return el
-    }
-})
-console.log(novasIdade)
+// const novasIdade = idades.filter((el) => {
+//   if (el >= 18) {
+//     return el;
+//   }
+// });
+// console.log(novasIdade);
 
-
-console.log("******************************************")
-for(n of idades){
-    if(n >= 18){
-        console.log(n)
+// console.log("******************************************");
+// for (n of idades) {
+//   if (n >= 18) {
+//     console.log(n);
+//   }
+// }
+for(valores of produtos){
+    if(valores.temDesconto){
+        valores.qtdProduto += 10
+        console.log(valores)
     }
 }
