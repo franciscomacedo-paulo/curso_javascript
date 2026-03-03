fb33e44fdfc2701f8abe84942ab9c77bb41fd3c2
Francisco Macedo <francarmando53@gmail.com>
2026-03-03 22:00:10 +0100

    Primeira parte dos teste feitos

--- modulo_avançado/aula06_pratica_com_filter.js
+++ modulo_avançado/aula06_pratica_com_filter.js
@@ -13,24 +13,35 @@ cursos.forEach((el)=>{
     novaDiv.setAttribute('class', 'curso')
     caixaCuros.appendChild(novaDiv)
     novaDiv.innerHTML = el
+
+    const divRadio = document.createElement('div')
+    divRadio.setAttribute('class', 'dradio')
+    divRadio.setAttribute('id', 'dr')
+    novaDiv.appendChild(divRadio)
+
     
-    //* CRIAÇÃO DO INPU TO TTIPO RADIO
+    //* CRIAÇÃO DO INPUt TO TTIPO RADIO
     const btnRadios = document.createElement('input')
     btnRadios.setAttribute('type', 'radio')
     btnRadios.setAttribute('name', 'rb_cursos')
-    novaDiv.appendChild(btnRadios)
+    divRadio.appendChild(btnRadios)
+    
 
 })
 
 // Button para ver o curso que esta sendo selecionado
 btn_curso_sel.addEventListener('click', ()=>{
     const rbC = [...document.querySelectorAll('input[type=radio]')]
-    const rdbSelect = rbC.filter((radio)=>{
-        if(radio.checked){
-            return radio
+
+    //** USANDO LOOP FOR PARA VER QUAL BOTAO RADIO FOI CLICADO
+    for(selecionado of rbC){
+        if(selecionado.checked === true){
+            console.log(selecionado.parentNode.parentNode.textContent)
         }
-    })    
-    console.log(rbC)
-    console.log(rdbSelect)
-    
+    }
+    //** USANDO METODO FILTER PARA VER QUAL BOTAO RADIO FOI SELECIOANDO
+    let radioChecked = rbC.filter((el)=>{
+            return el.checked
+    })
+    console.log(radioChecked[0].parentNode.previousSibling.textContent)
 })
