ee4199dc550ef9031393e9f8097244b8ea7a4b1e
Francisco Macedo <francarmando53@gmail.com>
2026-03-10 21:22:11 +0100

    Teste realizados com o metodo charAt

--- metodos_de_string/aula02_charAt.js
+++ metodos_de_string/aula02_charAt.js
@@ -1,3 +1,15 @@
 /*
 * Método que retorna um caracter num indeice(posicao especifica)
 */
+const frase = "Olá meu amor"
+const charFounded = frase.charAt(1)
+console.log(charFounded)
+
+const nome = "Francisco"
+for(n of nome){
+    if(n == 'a'){
+        console.log(n)
+        break
+    }
+}
+console.log(typeof(nome))
