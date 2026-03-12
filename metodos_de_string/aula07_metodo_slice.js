0834ced84baa6cd48b09d10a05e2afc17cfc100b
Francisco Macedo <francarmando53@gmail.com>
2026-03-12 17:54:06 +0100

    .

---
+++ metodos_de_string/aula07_metodo_slice.js
@@ -0,0 +1,9 @@
+const frase = "Apple, Banana,  kiwi"
+const animais = ["Avestros", "Boi","Camelo","Cavalo"]
+
+const extraidoDaFrase = frase.slice(7, 13)
+console.log(extraidoDaFrase)
+
+//* Repartir com valores negativos
+const rtl = animais.slice(-2)
+console.log(rtl)
