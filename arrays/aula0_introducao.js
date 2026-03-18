4e91ea7dea091e83b0212b1547b1444169aadc07
Francisco Macedo <francarmando53@gmail.com>
2026-03-18 23:30:35 +0100

    .

---
+++ arrays/aula0_introducao.js
@@ -0,0 +1,15 @@
+//Vamos começar a falar de arrays e object em JavaScript
+
+/*
+    * Arrays sao conjunto de dado com se fosse uma lista, de compras que tu usas quando vais ao super mercado
+*/
+
+const arrNumeros = [1,2,3,4,5,6]
+const arrString = ["Lurdes","Miguel","Milena","Mariana"]
+const arrBoolean = [true, false, true]
+const arrMisto = [1,3,-4,"Alexa",false]
+
+console.log(arrNumeros)
+console.log(arrString)
+console.log(arrBoolean)
+console.log(arrMisto)
