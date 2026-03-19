4dc115c5cd75f714a2f7bef2c45ae5b894001f7e
Francisco Macedo <francarmando53@gmail.com>
2026-03-19 01:09:40 +0100

    .

---
+++ arrays/aula01_acessando_itens.js
@@ -0,0 +1,5 @@
+const frutas = ['limao','manga','uva','goiaba','ananas','abacate']
+const radomNumeros = Math.floor(Math.random() * (7 -1))
+console.log(frutas[radomNumeros])
+console.log(frutas[2])
+console.log(frutas.length -1)
