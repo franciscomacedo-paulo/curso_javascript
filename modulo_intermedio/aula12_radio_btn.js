c78769933046466d6c268d877c3133ae0491efe0
Francisco Macedo <francarmando53@gmail.com>
2026-03-22 17:57:26 +0100

    .

--- modulo_intermedio/aula12_radio_btn.js
+++ modulo_intermedio/aula12_radio_btn.js
@@ -2,40 +2,46 @@ const radioButton = [...document.querySelectorAll('input[name="calculo"]')];
 const btn_mostrar = document.getElementById("btn");
 const primeiro_valor = document.querySelector("#valor1");
 const segundo_valor = document.querySelector("#valor2");
-const resultado = document.querySelector('#result')
+const resultado = document.querySelector("#result");
 
-
-function calculo(v1=0, sinal,v2=0){
-    let s = Number(v1) + Number(v2)
-    resultado.innerHTML = s
-
+function calculo(v1 = 0, sinal, v2 = 0) {
+  let s = Number(v1) + Number(v2);
+  resultado.innerHTML = s;
 }
-function verificarSelecionado(){
-    let radioSelecionado = '';
-    radioButton.forEach((el)=>{
-        if(el.checked){
-            radioSelecionado = el.value   
+function verificarSelecionado() {
+  let radioSelecionado = "";
+
+  radioButton.forEach((el) => {
+    if (el.checked) {
+      radioButton.addEventListener("change", (e) => {
+        if (this.checked) {
+          console.log(this.value);
         }
-    })
-    if(radioSelecionado === 'Soma'){
-        //calculo(primeiro_valor.value,segundo_valor.value)
-        let s = Number(primeiro_valor.value) + Number(segundo_valor.value)
-        resultado.innerHTML = s
+      });
+      radioSelecionado = el.value;
+    }
+  });
 
-    }else if(radioSelecionado === 'ubtração'){
-        let s = Number(primeiro_valor.value) - Number(segundo_valor.value)
-        resultado.innerHTML = s
-
-    }else if(radioSelecionado === 'Multiplicar'){
-        let s = Number(primeiro_valor.value) * Number(segundo_valor.value)
-        resultado.innerHTML = s
-    }    
-    return radioSelecionado
+  if (radioSelecionado === "Soma") {
+    let s = Number(primeiro_valor.value) + Number(segundo_valor.value);
+    resultado.innerHTML = s;
+  } else if (radioSelecionado === "ubtração") {
+    let s = Number(primeiro_valor.value) - Number(segundo_valor.value);
+    resultado.innerHTML = s;
+  } else if (radioSelecionado === "Multiplicar") {
+    let s = Number(primeiro_valor.value) * Number(segundo_valor.value);
+    resultado.innerHTML = s;
+  }
+  return radioSelecionado;
 }
 
+// radioButton.addEventListener("change", (e) => {
+//   if(this.checked){
+//     console.log(this.value)
+//   }
+// });
 
-btn_mostrar.addEventListener('click', (evt) =>{
-    evt.preventDefault()
-    console.log(verificarSelecionado())
-})
-
+btn_mostrar.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  console.log(verificarSelecionado());
+});
